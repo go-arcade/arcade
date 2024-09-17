d1b9246494433e88aeac6bee3259cbc0b20c7a8d
许怀安 <keington@outlook.com>
2024-09-17 22:40:22 +0800

    :iphone: add http response code and interceptor.

---
+++ pkg/httpx/http_code.go
@@ -0,0 +1,45 @@
+package httpx
+
+/**
+ * @author: gagral.x@gmail.com
+ * @time: 2024/9/16 21:25
+ * @file: http_code.go
+ * @description:
+ */
+
+var (
+	Failed = failed(500, "request failed")
+
+	Unauthorized           = failed(4001, "unauthorized")
+	AuthorizationIncorrect = failed(4002, "The auth format in the request header is incorrect")
+	AuthorizationEmpty     = failed(4003, "Authorization is empty")
+	TokenInvalid           = failed(4004, "Token is invalid")
+	TokenEmpty             = failed(4005, "Token is empty")
+
+	ErrUserPhone = failed(10001, "用户手机号不合法")
+	ErrSignParam = failed(10002, "签名参数有误")
+
+	InternalError = failed(5000, "internal error")
+)
+
+var (
+	Success = success(200, "success")
+)
+
+// failed 构造函数
+func failed(code int, msg string) *Response {
+	return &Response{
+		Code:   code,
+		Msg:    msg,
+		Detail: nil,
+	}
+}
+
+// success 构造函数
+func success(code int, msg string) *Response {
+	return &Response{
+		Code:   code,
+		Msg:    msg,
+		Detail: nil,
+	}
+}
---
+++ pkg/httpx/http_rep.go
@@ -0,0 +1,53 @@
+package httpx
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+/**
+ * @author: gagral.x@gmail.com
+ * @time: 2024/9/16 21:24
+ * @file: http_rep.go
+ * @description:
+ */
+
+type Response struct {
+	Code   int         `json:"code"`
+	Detail interface{} `json:"detail,omitempty"`
+	Msg    string      `json:"msg"`
+}
+
+// WithRepJSON 只返回json数据
+func WithRepJSON(c *gin.Context, detail interface{}) {
+	c.JSON(http.StatusOK, Response{
+		Code:   Success.Code,
+		Detail: detail,
+		Msg:    Success.Msg,
+	})
+}
+
+// WithRepMsg 返回自定义code, msg
+func WithRepMsg(c *gin.Context, code int, msg string) {
+	c.JSON(http.StatusOK, Response{
+		Code: code,
+		Msg:  msg,
+	})
+}
+
+// WithRepDetail 返回自定义code, msg, detail
+func WithRepDetail(c *gin.Context, code int, msg string, detail interface{}) {
+	c.JSON(http.StatusOK, Response{
+		Code:   code,
+		Detail: detail,
+		Msg:    msg,
+	})
+}
+
+// WithRepNotDetail 只成功的返回操作结果，返回结构体没有detail字段
+func WithRepNotDetail(c *gin.Context) {
+	c.JSON(http.StatusOK, Response{
+		Code: Success.Code,
+		Msg:  Success.Msg,
+	})
+}
