9f2496dc239a82d47f20861d1e889080d454c4d7
许怀安 <keington@outlook.com>
2024-09-21 16:48:29 +0800

    :truck: modify project dir.

--- internal/app/engine/logic/logic_agent.go
+++ internal/app/engine/logic/logic_agent.go
@@ -1,7 +1,8 @@
 package logic
 
 import (
-	"github.com/go-arcade/arcade/internal/app/basic/models"
+	"github.com/go-arcade/arcade/internal/app/engine/model"
+	"github.com/go-arcade/arcade/pkg/ctx"
 	"github.com/go-arcade/arcade/pkg/id"
 	"time"
 )
@@ -13,8 +14,12 @@ import (
  * @description: agent logic
  */
 
-func AddAgent(agent *models.AddAgentReq) error {
-	a := models.Agent{}
+type AgentLogic struct {
+	Ctx ctx.Context
+}
+
+func AddAgent(agent *model.AddAgentReq) error {
+	a := model.Agent{}
 	agent.AgentId = id.GetUild()
 	agent.IsEnable = 1
 	agent.CreatAt = time.Now()
@@ -25,7 +30,7 @@ func AddAgent(agent *models.AddAgentReq) error {
 	return err
 }
 
-func UpdateAgent(agent *models.Agent) error {
+func UpdateAgent(agent *model.Agent) error {
 	var err error
 	if err = agent.Update(); err != nil {
 		return err
@@ -33,8 +38,8 @@ func UpdateAgent(agent *models.Agent) error {
 	return err
 }
 
-func ListAgent(pageNum, pageSize int) ([]models.Agent, int64, error) {
-	a := models.Agent{}
+func ListAgent(pageNum, pageSize int) ([]model.Agent, int64, error) {
+	a := model.Agent{}
 
 	offset := (pageNum - 1) * pageSize
 	agents, count, err := a.List(offset, pageSize)
