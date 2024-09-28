7932204a48da0410523b0cc03252d2fe83707e76
许怀安 <keington@outlook.com>
2024-09-28 17:00:14 +0800

    :sparkles: separate http and router.

---
+++ internal/app/engine/server/http.go
@@ -0,0 +1,122 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-arcade/arcade/pkg/ctx"
+)
+
+/**
+ * @author: gagral.x@gmail.com
+ * @time: 2024/9/8 15:38
+ * @file: http.go
+ * @description: http server
+ */
+
+type Http struct {
+	Host                string
+	Port                int
+	Mode                string
+	InternalContextPath string
+	ExternalContextPath string
+	Heartbeat           int64
+	PProf               bool
+	ExposeMetrics       bool
+	AccessLog           bool
+	ReadTimeout         int
+	WriteTimeout        int
+	IdleTimeout         int
+	ShutdownTimeout     int
+	TLS                 TLS
+	Auth                Auth
+	Ctx                 ctx.Context
+}
+
+type TLS struct {
+	CertFile string
+	KeyFile  string
+}
+
+type Auth struct {
+	AccessExpire   int64
+	RefreshExpire  int64
+	RedisKeyPrefix string
+}
+
+func NewHttp(cfg Http) *Http {
+	return &Http{
+		Host:                cfg.Host,
+		Port:                cfg.Port,
+		Mode:                cfg.Mode,
+		InternalContextPath: cfg.InternalContextPath,
+		ExternalContextPath: cfg.ExternalContextPath,
+		Heartbeat:           cfg.Heartbeat,
+		PProf:               cfg.PProf,
+		ExposeMetrics:       cfg.ExposeMetrics,
+		AccessLog:           cfg.AccessLog,
+		ReadTimeout:         cfg.ReadTimeout,
+		WriteTimeout:        cfg.WriteTimeout,
+		IdleTimeout:         cfg.IdleTimeout,
+		ShutdownTimeout:     cfg.ShutdownTimeout,
+		TLS:                 cfg.TLS,
+		Auth:                cfg.Auth,
+		Ctx:                 cfg.Ctx,
+	}
+}
+
+func (h *Http) Server(engine *gin.Engine) func() {
+	addr := fmt.Sprintf("%s:%d", h.Host, h.Port)
+
+	srv := &http.Server{
+		Addr:         addr,
+		Handler:      engine,
+		ReadTimeout:  time.Duration(h.ReadTimeout) * time.Second,
+		WriteTimeout: time.Duration(h.WriteTimeout) * time.Second,
+		IdleTimeout:  time.Duration(h.IdleTimeout) * time.Second,
+	}
+
+	go func() {
+		fmt.Println("[Init] http server start at:", addr)
+
+		if h.TLS.CertFile != "" && h.TLS.KeyFile != "" {
+			if err := srv.ListenAndServeTLS(h.TLS.CertFile, h.TLS.KeyFile); err != nil {
+				panic(err)
+			}
+		} else {
+			if err := srv.ListenAndServe(); err != nil {
+				panic(err)
+			}
+		}
+	}()
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	return func() {
+
+		<-sc
+		fmt.Println("[shutdown] server is shutting down...")
+
+		c, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(h.ShutdownTimeout))
+		defer cancel()
+
+		srv.SetKeepAlivesEnabled(false)
+		if err := srv.Shutdown(c); err != nil {
+			fmt.Println("[shutdown] server shutdown error: ", err)
+		}
+
+		select {
+		case <-c.Done():
+			fmt.Println("[shutdown] server shutdown timeout of ", h.ShutdownTimeout, " seconds")
+		default:
+			fmt.Println("[shutdown] http exit...")
+		}
+	}
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,10 +1,18 @@
 package router
 
 import (
+	"embed"
+	"github.com/gin-contrib/pprof"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/go-arcade/arcade/internal/app/engine/logic"
+	"github.com/go-arcade/arcade/internal/app/engine/server"
+	"github.com/go-arcade/arcade/pkg/httpx"
+	"github.com/go-arcade/arcade/pkg/httpx/interceptor"
 	"github.com/go-arcade/arcade/pkg/httpx/ws"
-	_ "github.com/go-kratos/kratos/v2/log"
-	_ "github.com/rakyll/statik/fs"
+	"github.com/go-arcade/arcade/pkg/version"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
 )
 
 /**
@@ -15,24 +23,79 @@ import (
  */
 
 type Router struct {
+	Http       *server.Http
+	AgentLogic logic.AgentLogic
 }
 
-func EngineRouter(r *gin.RouterGroup) {
+//go:embed static
+var web embed.FS
 
-	//staticsFS, err := fs.New()
-	//if err != nil {
-	//	log.Errorf("cannot create statik fs: %v", err)
-	//}
-	//r.StaticFS("/static", staticsFS)
+func NewRouter(cfg *server.Http) *Router {
+	return &Router{
+		Http:       cfg,
+		AgentLogic: logic.AgentLogic{},
+	}
+}
+
+func (ar *Router) Router() *gin.Engine {
+
+	gin.SetMode(ar.Http.Mode)
+
+	r := gin.New()
+
+	// panic recover
+	r.Use(interceptor.ExceptionInterceptor)
+
+	// unified response interceptor
+	r.Use(interceptor.UnifiedResponseInterceptor())
+
+	// web static resource
+	r.Use(static.Serve("/", static.EmbedFolder(web, "static")))
+	r.NoRoute(func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/")
+	})
+
+	if ar.Http.AccessLog {
+		r.Use(gin.LoggerWithFormatter(httpx.AccessLogFormat))
+	}
+
+	if ar.Http.PProf {
+		pprof.Register(r, "/debug/pprof")
+	}
+
+	if ar.Http.ExposeMetrics {
+		r.GET("/metrics", gin.WrapH(promhttp.Handler()))
+	}
+
+	r.GET("/health", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+	r.GET("/version", func(c *gin.Context) {
+		c.JSON(http.StatusOK, version.GetVersion())
+	})
+
+	// core router, internal api router
+	core := r.Group(ar.Http.InternalContextPath)
+	{
+		// engine router
+		ar.RouterGroup(core)
+	}
+
+	return r
+}
+
+func (ar *Router) RouterGroup(r *gin.RouterGroup) *gin.RouterGroup {
 
 	r.POST("/ws", ws.Handle)
 
 	route := r.Group("/agent")
 	{
-		route.POST("/add", addAgent)
+		route.POST("/add", ar.addAgent)
 		//r.POST("delete", deleteAgent)
 		//r.POST("update", updateAgent)
-		route.GET("/list", listAgent)
+		route.GET("/list", ar.listAgent)
 	}
 
+	return route
 }
