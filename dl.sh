9829255b1da85af9d93dc54e6d554fe79dafe6be
许怀安 <keington@outlook.com>
2024-09-28 17:03:06 +0800

    :ambulance: use gin own embed to package web static resources.

--- dl.sh
+++ dl.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
-if [ ! -d "./pub" ]; then
-    TAG=$(curl -sX GET https://api.github.com/repos/gagraler/fe/releases/latest   | awk '/tag_name/{print $4;exit}' FS='[""]')
+target_dir="./internal/router/static"
 
-    if ! curl -o n9e-fe-${TAG}.tar.gz -L https://github.com/gagraler/fe/releases/download/${TAG}/n9e-fe-${TAG}.tar.gz; then
-        echo "failed to download n9e-fe-${TAG}.tar.gz!"
-        exit 1
-    fi
-
-    if ! tar zxf n9e-fe-${TAG}.tar.gz; then
-        echo "failed to untar n9e-fe-${TAG}.tar.gz!"
-        exit 2
-    fi
+if [ ! -d $target_dir ]; then
+    mkdir -p $target_dir
 fi
 
-GOPATH=$(go env GOPATH)
-GOPATH=${GOPATH:-/root/go}
+tag=$(curl -sX GET https://api.github.com/repos/go-arcade/docs/releases/latest | awk '/tag_name/{print $4;exit}' FS='[""]')
 
-# Embed files into a go binary
-# go install github.com/rakyll/statik
-if ! $GOPATH/bin/statik -src=./static; then
-    echo "failed to embed files into a go binary!"
-    exit 4
+if ! curl -o dist.zip -L "https://github.com/go-arcade/docs/releases/download/${TAG}/dist.zip"; then
+    echo "Failed to download ${tag} dist.zip"
+    exit 1
 fi
+
+if ! unzip -d "target_dir" dist.zip; then
+    echo "Failed to unzip ${tag} dist.zip into ${target_dir}"
+    exit 2
+fi
+
+rm -f dist.zip
