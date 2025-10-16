e66db2e6d6910cf79ff97438a35e055c8e9c2280
gagral <keington@outlook.com>
2025-10-16 15:33:00 +0800

    refactor: 移除 Kafka 日志写入功能
    - 删除 `log_kafka` 文件，移除与 Kafka 日志写入相关的实现。
    - 更新 `log.go` 文件，移除对 Kafka 日志写入器的引用，简化日志配置逻辑。
    - 修改日志级别解析函数为小写，以提高一致性和可读性。

--- pkg/log/log_level.go
+++ pkg/log/log_level.go
@@ -1,12 +1,5 @@
 package log
 
-/**
- * @author: gagral.x@gmail.com
- * @time: 2024/9/16 15:34
- * @file: log_level.go
- * @description: LogConfig level
- */
-
 // LogLevel defines the severity of a LogConfig internal.
 type LogLevel int8
 
@@ -23,35 +16,18 @@ const (
 	FatalLevel
 )
 
-func (l LogLevel) String() string {
-	switch l {
-	case DebugLevel:
-		return "debug"
-	case InfoLevel:
-		return "info"
-	case WarnLevel:
-		return "warn"
-	case ErrorLevel:
-		return "error"
-	case FatalLevel:
-		return "fatal"
-	default:
-		return "unknown"
-	}
-}
-
-// ParseLogLevel converts a string level to a LogLevel.
-func ParseLogLevel(level string) LogLevel {
+// parseLogLevel converts a string level to a LogLevel.
+func parseLogLevel(level string) LogLevel {
 	switch level {
-	case "debug":
+	case "DEBUG":
 		return DebugLevel
-	case "info":
+	case "INFO":
 		return InfoLevel
-	case "warn":
+	case "WARN":
 		return WarnLevel
-	case "error":
+	case "ERROR":
 		return ErrorLevel
-	case "fatal":
+	case "FATAL":
 		return FatalLevel
 	default:
 		return InfoLevel
